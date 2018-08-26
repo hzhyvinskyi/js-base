bbccb4dba46fbe4977f404fe88b01c16087a6620
hzhyvinskyi <32293052+hzhyvinskyi@users.noreply.github.com>
2018-08-26 10:44:52 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,34 +2,34 @@ var canvas = document.getElementById('canvas');
 
 var ctx = canvas.getContext('2d');
 
-// var gradient = ctx.createRadialGradient(150, 70, 0, 100, 200, 70);
-//
-// gradient.addColorStop(0, 'Gold');
-// gradient.addColorStop(1, 'rgba(0, 150, 0, 0)');
-//
-// ctx.fillStyle = gradient;
-// ctx.fillRect(0, 0, 600, 300);
-//
-// ctx.strokeStyle = 'silver';
-// ctx.strokeRect(150, 75, 50, 50);
-//
-// ctx.clearRect(0, 0, 300, 150);
+var gradient = ctx.createRadialGradient(150, 70, 0, 100, 200, 70);
 
-// ctx.beginPath();
-// ctx.strokeStyle = 'black';
-// ctx.moveTo(100, 100);
-// ctx.lineTo(80, 60);
-// ctx.lineTo(110, 80);
-// ctx.lineTo(125, 40);
-// ctx.lineTo(140, 80);
-// ctx.lineTo(170, 60);
-// ctx.lineTo(150, 100);
-// ctx.lineTo(150, 100);
-// ctx.bezierCurveTo(140, 90, 110, 90, 100, 100);
-// ctx.closePath();
-// ctx.stroke();
-// ctx.fillStyle = 'gold';
-// ctx.fill();
+gradient.addColorStop(0, 'Gold');
+gradient.addColorStop(1, 'rgba(0, 150, 0, 0)');
+
+ctx.fillStyle = gradient;
+ctx.fillRect(0, 0, 600, 300);
+
+ctx.strokeStyle = 'silver';
+ctx.strokeRect(150, 75, 50, 50);
+
+ctx.clearRect(0, 0, 300, 150);
+
+ ctx.beginPath();
+ctx.strokeStyle = 'black';
+ctx.moveTo(100, 100);
+ctx.lineTo(80, 60);
+ctx.lineTo(110, 80);
+ctx.lineTo(125, 40);
+ctx.lineTo(140, 80);
+ctx.lineTo(170, 60);
+ctx.lineTo(150, 100);
+ctx.lineTo(150, 100);
+ctx.bezierCurveTo(140, 90, 110, 90, 100, 100);
+ctx.closePath();
+ctx.stroke();
+ctx.fillStyle = 'gold';
+ctx.fill();
 
 ctx.beginPath();
 ctx.moveTo(150, 0);
